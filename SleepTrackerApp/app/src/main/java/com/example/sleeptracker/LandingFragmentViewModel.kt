1ade9ffd1024c1c3dffe8302217265bf888ad307
nmakhari <nmakhari@uwaterloo.ca>
2020-08-27 03:17:24 -0400

    Initialized viewModel for landing fragment

--- SleepTrackerApp/app/src/main/java/com/example/sleeptracker/LandingFragmentViewModel.kt
+++ SleepTrackerApp/app/src/main/java/com/example/sleeptracker/LandingFragmentViewModel.kt
@@ -1,6 +1,17 @@
 package com.example.sleeptracker
 
 import androidx.lifecycle.ViewModel
+import timber.log.Timber
 
 class LandingFragmentViewModel: ViewModel() {
+
+    init {
+        Timber.d("LandingFragmentViewModel created")
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        // TODO: clean up the viewModels resources here, before they're gone for good
+        Timber.d("LandingFragmentViewModel destroyed")
+    }
 }
