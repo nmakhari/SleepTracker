e1a52653356af296cb9be2ac8c30b8bb67bb5af1
nmakhari <nmakhari@uwaterloo.ca>
2020-09-14 00:06:24 -0400

    Implemented placeholder click listeners, starting to integrate rating LiveData

--- SleepTrackerApp/app/src/main/java/com/example/sleeptracker/RatingFragment.kt
+++ SleepTrackerApp/app/src/main/java/com/example/sleeptracker/RatingFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.sleeptracker.databinding.FragmentRatingBinding
 import timber.log.Timber
@@ -22,6 +23,10 @@ class RatingFragment : Fragment() {
         FragmentRatingbinding = FragmentRatingBinding.inflate(layoutInflater)
         viewModel = ViewModelProvider(this).get(RatingFragmentViewModel::class.java)
         setClickListeners()
+
+        viewModel.currentRatingImage.observe(viewLifecycleOwner, Observer { newRating ->
+            FragmentRatingbinding.selectedRating.setImageResource(newRating)
+        })
         return FragmentRatingbinding.root
     }
 
@@ -29,7 +34,22 @@ class RatingFragment : Fragment() {
         FragmentRatingbinding.submitButton.setOnClickListener {
             Timber.d("Submit rating button clicked")
         }
-        // TODO: Implement rating click listeners and changes in viewModel
+
+        // TODO: Refactor these listeners into the view itself
+        FragmentRatingbinding.goodRating.setOnClickListener{
+            Timber.d("Good rating selected")
+            viewModel.currentRatingImage.value = R.drawable.good_rating
+        }
+
+        FragmentRatingbinding.neutralRating.setOnClickListener{
+            Timber.d("Neutral rating selected")
+            viewModel.currentRatingImage.value = R.drawable.neutral_rating
+        }
+
+        FragmentRatingbinding.poorRating.setOnClickListener{
+            Timber.d("Poor rating selected")
+            viewModel.currentRatingImage.value = R.drawable.poor_rating
+        }
     }
 
 }
--- SleepTrackerApp/app/src/main/java/com/example/sleeptracker/RatingFragmentViewModel.kt
+++ SleepTrackerApp/app/src/main/java/com/example/sleeptracker/RatingFragmentViewModel.kt
@@ -1,12 +1,20 @@
 package com.example.sleeptracker
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import timber.log.Timber
 
 class RatingFragmentViewModel: ViewModel() {
+    // This variable will contain an image resource, hence int type
+    val currentRatingImage = MutableLiveData<Int>()
+
     init {
         Timber.d("RatingViewModel created")
+        currentRatingImage.value = 0
     }
+    // Needs current image
+    // Needs to load in the sleeping time
+    // needs to interact with the view to directly implement listeners
     override fun onCleared() {
         super.onCleared()
         // TODO: clean up resources on viewModel destruction
